a0025c95be1b61838031cb249d0ef582af3f6bcd
ImsirovicAjdin <ajdika@live.com>
2019-09-05 13:13:01 +0200

    The completed checkbox textarea example

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -4,4 +4,21 @@ import { Component } from '@angular/core';
   selector: 'the-table',
   templateUrl: './table.component.html'
 })
-export class TableComponent {}
+export class TableComponent {
+
+  public isChecked = true;
+  public userComment: string = "Just some placehlder text";
+
+  toggleCheckbox(e) {
+    if (!this.isChecked) {
+      if(confirm("Clicking OK will erase your comment, are you sure?")){
+        this.isChecked = !this.isChecked;
+        this.userComment = "Just some placehlder text";
+      } else {
+        e.preventDefault();
+      }
+      return
+    }
+    this.isChecked = !this.isChecked
+  }
+}
